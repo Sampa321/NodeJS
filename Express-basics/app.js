71903fc440ad06ea687e31f8f41f7f62cb77fce3
Sampa Nayak <sampanayak987@gmail.com>
2026-06-21 21:20:23 +0530

    msg

---
+++ Express-basics/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+const app = express()   //app -> object or references
+app.use((req, res) =>{
+    // res.send("Hello from express js")
+
+    person ={
+        name : "Sampa",
+        age : 21
+    }
+    res.send(person)
+})
+app.listen(3000)
