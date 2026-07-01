b5b57232f382513f9ebd36c267b3b2d7409cbc8d
Sampa Nayak <sampanayak987@gmail.com>
2026-07-01 21:58:35 +0530

    msg

--- Express-basics/app.js
+++ Express-basics/app.js
@@ -15,27 +15,94 @@ const app = express()   //app -> object or references
 // })
 
 
-let product = {
-    "name" : "dress",
-    "Price" : 3000
+
+
+
+
+
+// let product = {
+//     "name" : "dress",
+//     "Price" : 3000
+// }
+
+// app.get("/Home",(req,res)=>{
+//     res.send("It is Home page!!")
+// })
+
+// app.get("/about",(req,res)=>{
+//     res.send("It is about page!!")
+// })
+
+// app.get("/skill",(req,res)=>{
+//     res.send("It is skill page!!")
+// })
+
+// app.get("/product",(req,res)=>{
+//     // res.send("It is product page!!")
+//     res.send(product)
+// })
+
+
+
+
+
+
+const fn = (req,res,next)=>{
+    //res.send("welcome")
+    console.log("welcome");
+    next()
 }
 
-app.get("/Home",(req,res)=>{
-    res.send("It is Home page!!")
-})
+const fn2 = (req,res,next)=>{
+    //res.send("welcome fn2")
+    console.log("Welcome fn2");
+    next()
+}
 
-app.get("/about",(req,res)=>{
-    res.send("It is about page!!")
-})
+const fn3 = (req,res)=>{
+    res.send("welcome fn3")
+}
 
-app.get("/skill",(req,res)=>{
-    res.send("It is skill page!!")
-})
+app.get("/",fn,fn2,fn3)
 
-app.get("/product",(req,res)=>{
-    // res.send("It is product page!!")
-    res.send(product)
-})
+//OR,
+//app.get("/",fn)
+//app.get("/",fn2)
+//app.get("/",fn3)
+
+
+
+
+
+// let users = []
+// app.get("/signup",(req,res)=>{
+//     users.push(req.body)
+//     res.status(201).send("received")
+// })
+
+// app.get("/users", (req,res)=>{
+//     res.send(users)
+// })
+
+
+// app.get("/delete",(req,res)=>{
+//     users = []
+//     req.send("users deleted")
+// })
+
+// app.get("/login",(req,res)=>{
+//     let name = req.body.name
+//     let password = req.body.password
+
+//     for(let user of users)
+//     {
+//         if(user.name == name && user.password == password)
+//         {
+//             return res.send("Login successfull")
+//         }
+//     }
+//     return res.status(401).send("Login failed")
+// })
 
 app.listen(3000,()=>{
     console.log("Server is running on port 30000");
